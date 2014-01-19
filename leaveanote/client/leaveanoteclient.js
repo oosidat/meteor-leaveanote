3ab925f9752aa7985345d52cf9a16f6795f60d25
Osama Sidat <oosidat@gmail.com>
2014-01-18 19:06:40 -0500

    Added functions for submitting and getting notes; started CSS modification

--- leaveanote/client/leaveanoteclient.js
+++ leaveanote/client/leaveanoteclient.js
@@ -2,10 +2,19 @@ Template.addnote.events({
     'click input.add-note' : function(event){
         event.preventDefault();
         var noteText = document.getElementById("noteInput").value;
+        
         Meteor.call("addNote",noteText,function(error , noteId){
           console.log('added note with Id .. '+noteId);
         });
+        
         document.getElementById("noteInput").value = "";
  
     }
 });
+
+
+Notes = new Meteor.Collection("notes");
+
+Template.notes.items = function(){
+    return Notes.find({},{sort:{'submittedOn':-1}});
+}

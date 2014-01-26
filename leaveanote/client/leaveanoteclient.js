b9b7f49da1e3458a718a57a03a7d0e701097a6ab
Osama Sidat <oosidat@gmail.com>
2014-01-25 19:01:33 -0500

    Moved dateformatting to client, saving note submit time as UTC on server. May have fixed client side date time annoyance.

--- leaveanote/client/leaveanoteclient.js
+++ leaveanote/client/leaveanoteclient.js
@@ -1,3 +1,13 @@
+Notes = new Meteor.Collection("notes");
+
+
+/* Get Notes */
+Template.notes.items = function(){
+    return Notes.find({},{sort:{'submittedOn':-1}});
+}
+
+
+/* Add note */
 Template.addnote.events({
 	'click #add-note-button' : function(event){
 		event.preventDefault();
@@ -21,8 +31,32 @@ Template.addnote.events({
 });
 
 
-Notes = new Meteor.Collection("notes");
+/* Nicely formatted Date */
+Template.note.helpers({
+	localDateTime: function() {
+		
+		var utcTime = this.submittedOn;
+		return formatAMPM(utcTime);
+	}
 
-Template.notes.items = function(){
-    return Notes.find({},{sort:{'submittedOn':-1}});
+});
+
+var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+
+var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
+
+function formatAMPM(dateObj) {
+	var day = dayNames[dateObj.getDay()];
+	var date = dateObj.getDate();
+	var month = monthNames[dateObj.getMonth()];
+	var year = dateObj.getFullYear();
+	var hours = dateObj.getHours();
+	var minutes = dateObj.getMinutes();
+	var ampm = hours >= 12 ? 'pm' : 'am';
+	hours = hours % 12;
+	hours = hours ? hours : 12; // the hour '0' should be '12'
+	minutes = minutes < 10 ? '0'+minutes : minutes;
+	var strTime = day + ", " + month + " " + date + " " + year + ", " + hours + ':' + minutes + ' ' + ampm;
+	return strTime;
 }
--- leaveanote/server/leaveanoteserver.js
+++ leaveanote/server/leaveanoteserver.js
@@ -4,39 +4,33 @@ Meteor.startup(function () {
 // code to run on server at startup
 });
 
-var colours = ["yellowsticky", "pinksticky", 'bluesticky', 'greensticky']
+var colours = ["yellowsticky", "pinksticky", 
+'bluesticky', 'greensticky']
 
 Meteor.methods({
 	addNote : function(noteText){
 		console.log("Adding your note!");
-		var today = new Date();
-		var colour = colours[Math.floor(Math.random()*colours.length)];
+		var colour = colours[Math.floor(
+			Math.random()*colours.length)];
+		
 		var noteId = Notes.insert({
 			'noteText' : noteText,
-			'submittedOn' : today,
-			'formattedDate': formatAMPM(today),
+			'submittedOn' : UtcDateTimeNow(),
 			'stickyColor': colour
 		});
 		return noteId;
 	}
 });
 
-var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+function UtcDateTimeNow() {
+	var now = new Date();
+	var now_utc = new Date(
+		now.getUTCFullYear(), 
+		now.getUTCMonth(), 
+		now.getUTCDate(), 
+		now.getUTCHours(), 
+		now.getUTCMinutes(), 
+		now.getUTCSeconds());
 
-var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
-
-function formatAMPM(dateObj) {
-	var day = dayNames[dateObj.getDay()];
-	var date = dateObj.getDate();
-	var month = monthNames[dateObj.getMonth()];
-	var year = dateObj.getFullYear();
-	var hours = dateObj.getHours();
-	var minutes = dateObj.getMinutes();
-	var ampm = hours >= 12 ? 'pm' : 'am';
-	hours = hours % 12;
-	hours = hours ? hours : 12; // the hour '0' should be '12'
-	minutes = minutes < 10 ? '0'+minutes : minutes;
-	var strTime = day + ", " + month + " " + date + " " + year + ", " + hours + ':' + minutes + ' ' + ampm;
-	return strTime;
+	return now_utc;
 }
